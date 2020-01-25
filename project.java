90f26188f649a158cadecb3b615623c31001f3ae
Suryansh Tiwari <tsuryansh97@gmail.com>
2020-01-25 12:12:05 +0530

    1st  Commit 		Containing name of Class(Project)

---
+++ project.java
@@ -0,0 +1,18 @@
+
+package learnJava;
+
+import java.util.*;
+
+class project{
+
+    public static void main(String[] args){
+
+    	Scanner scn = new Scanner(System.in);
+        System.out.println("Hello World, Lets Start the Game");
+
+    }
+}
+
+
+
+
