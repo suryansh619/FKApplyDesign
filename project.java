4adff6e184fb04db88edff8dbc430c1c7193a027
Suryansh Tiwari <tsuryansh97@gmail.com>
2020-01-25 12:53:33 +0530

    Added Players Class which implements Human(which is basically interface for human and machine)

--- project.java
+++ project.java
@@ -63,6 +63,25 @@ class Grid {
 	}
 
 }
+interface Human {
+	String getType();
+	String getName();
+	char getSymbol();
+}
+class Player implements Human{
+	private String Name, Type;
+	private char Symbol;
+
+	Player(String name,String type,char symbol){
+		this.Name = name;
+		this.Type = type;
+		this.Symbol = symbol;
+	}
+
+	public String getType(){ return this.Type; }
+	public String getName(){ return this.Name; }
+	public char getSymbol(){ return this.Symbol; }
+}
 
 class project{
 
@@ -87,6 +106,36 @@ class project{
         temp_y = scn.nextInt();
         
         Grid grid = new Grid(temp_x,temp_y);
+
+
+        int no_of_players;
+        // no of players
+        System.out.println("How Many Players are there ?");
+        no_of_players = scn.nextInt();
+
+        Player[] players = new Player[no_of_players];
+
+        //name + type + symbol
+        for(int i=0;i<=no_of_players;i++)
+        {
+        	String temp_str;
+        	temp_str = scn.nextLine();
+        	if(i==0)
+        		continue;
+        	String[] arr = temp_str.split(" ",3);
+        	
+        	players[i-1] = new Player(arr[0],arr[1],arr[2].charAt(0));
+
+        }
+        
+        
+
+        //Order of Players
+        Deque<Player> deque = new ArrayDeque<Player>();
+        for(int i=1;i<no_of_players;i++)
+        {
+        	deque.addFirst(players[i]);
+        }
     }
 }
 
